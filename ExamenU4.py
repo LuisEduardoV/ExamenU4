575bad91154ac82afbc83b62e62f9a7bcdd1b7d7
LuisEduardoV <L18TE0341@teziutlan.tecnm.mx>
2021-11-13 02:23:06 -0600

    Nuevo commit1

--- ExamenU4.py
+++ ExamenU4.py
@@ -1,14 +1,25 @@
 """
-
-Estos son los cambios realizados desde pycharm
-
+Examen unidad 4
+En este programa se verá la interacción de un
+generente al entrevistar a una persona que sera nuevo empleado
 """
-class Gerente:
-    def __init__(self, nombre, edad, sexo, estatura = ('1.80m')):
+class Empresa:
+    def __init__(self,NombreEmbresa):
+        self.__NombreEmpresa = NombreEmbresa
+
+class Empleado(Empresa):
+    def __init__(self, puesto):
+        self.__puesto = puesto
+
+
+class Gerente(Empleado):
+    def __init__(self, nombre, sexo,puesto,edad =('25'), estatura = ('1.80m')):
+        super().__init__(puesto)
         self.__nombre = nombre
         self.__edad = edad
         self.__sexo = sexo
         self.__estatura = estatura
+
 
     def getNombre(self):
         self.__nombre()
@@ -20,18 +31,33 @@ class Gerente:
         self.__estatura()
 
     def info(self):
-        print("El nombre del generte es: ", self.__nombre)
-        print("La edad del gerente es: ", self.__edad)
-        print("El sexo del gerente es: ", self.__sexo)
-        print("La estatura del generte es: ",self.__estatura)
+        print("Hola mi nombre es: ", self.__nombre)
+        print("Tengo", self.__edad,"años")
+        print("Mi genero es: ", self.__sexo)
+        print("Y mi estatura es: ",self.__estatura)
 
-Nombre = input("Ingrese un nombre para el generte: ")
-Edad= input("Ingrese la edad del generte: ")
-Sexo= input(": ")
+print("Hola vienvenido a la empresa TRES HERMANOS ")
+condicion = input("Hola qué tal, si usted desea ingresar a nuestra empresa presione la tecla 1,"
+                " sino presiones cualquier tecla")
+if (condicion == '1'):
+    print("Hola usted a decidido ingresar a nuestra empresa,")
+    print("ahora vendra un personal para atenderlo")
+    Nombre = input("Porfavor ingrese el nombre de la persona que lo atendera: ")
+    Sexo = input("Porfavor ingrese el genero de la persona que desee que lo atienda: ")
+    Puesto = input("El puesto es: ")
+
+    gerente1 = Gerente(Nombre, Sexo,Puesto)
+    gerente1.info()
+else:
+    print("Hasta luego, que tenga exelente día")
 
 
-gerente1 = Gerente(Nombre,Edad,Sexo)
-gerente1.info()
+
+
+
+
+
+
 
 
 
