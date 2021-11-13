27ce937afe8193b5e45c22e8bc543b186fbb46f1
LuisEduardoV <L18TE0341@teziutlan.tecnm.mx>
2021-11-13 10:16:35 -0600

    Nuevo commit3

--- ExamenU4.py
+++ ExamenU4.py
@@ -6,6 +6,7 @@ generente al entrevistar a una persona que sera nuevo empleado
 class Empresa:
     def __init__(self,NombreEmbresa):
         self.__NombreEmpresa = NombreEmbresa
+        pass
 
 class Empleado(Empresa):
     def __init__(self, puesto):
@@ -13,7 +14,6 @@ class Empleado(Empresa):
 
     def getPuesto(self):
         self.__puesto()
-
 
 class Gerente(Empleado):
     def __init__(self, nombre,puesto=None, sexo='hombre',edad ='25', estatura = '1.80m'):
@@ -35,17 +35,36 @@ class Gerente(Empleado):
     def info(self):
         print("Hola mi nombre es", self.__nombre,"tengo", self.__edad,"años")
         print("ocupo el puesto de gerente","soy", self.__sexo,"y mido",self.__estatura)
+class preguntas:
+    def __init__(self,Pregunta1,Pregunta2,Pregunta3,Pregunta4,Pregunta5):
+        self.__Pregunta1 = Pregunta1
+        self.__Pregunta2 = Pregunta2
+        self.__Pregunta3 = Pregunta3
+        self.__Pregunta4 = Pregunta4
+        self.__Pregunta5 = Pregunta5
+
+    def hacerEntrevista(self):
+        print("Ahora le voy hacer una entrevista....")
+
+
+class saco:
+    def __init__(self,medida,color,botones):
+        self.__medida=medida
+        self.__color=color
+        self.__botones=botones
 
 print("Hola vienvenido a la empresa TRES HERMANOS ")
 print("Si usted desea trabajar en la empresa presione la tecla 1")
 condicion = input("sino presione cualquier tecla: ")
+print("---------------------------------------------------------------------------------")
 if (condicion == '1'):
     print("Hola usted a decidido ingresar a nuestra empresa,")
     print("ahora vendra un personal para atenderlo")
     Nombre = input("Porfavor ingrese el nombre de la persona que lo atendera: ")
-
+    print("---------------------------------------------------------------------------------")
     gerente1 = Gerente(Nombre)
     gerente1.info()
+
 else:
     print("Hasta luego, que tenga exelente día")
 
